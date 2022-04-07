452fe3dfcdbdbf6fdb66d07a1948a59d891926d0
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-07 18:08:15 -0300

    tests: implementing tests for services layers of the endpoint /products/:id

--- test/unit/services/productsService.js
+++ test/unit/services/productsService.js
@@ -1,22 +1,59 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 
-const { products } = require('../data');
+const { products, product } = require('../stubs');
 const ProductsModel = require('../../../models/produtsModel');
 const ProductsService = require('../../../services/productsService');
 
-describe('Para o endpoint "/products"', () => {
-  before(() => {
-    sinon.stub(ProductsModel, 'getAll').resolves(products);
+describe('Testa a camada services de produtos"', () => {
+
+  describe('Ao fazer uma requisição ao endpoint /products', () => {
+    
+    before(() => {
+      sinon.stub(ProductsModel, 'getAll').resolves(products);
+    });
+  
+    after(() => {
+      ProductsModel.getAll.restore();
+    });
+  
+    it('retorna todos os dados de produtos', async () => {
+      const result = await ProductsService.getAll();
+  
+      expect(result).to.be.deep.equal(products)
+    });
+
   });
 
-  after(() => {
-    ProductsModel.getAll.restore();
-  });
+  describe('Ao fazer uma requisição ao endpoint /products/:id', () => { 
+     
+    describe('caso exista um produto com o id passado', () => {
 
-  it('retorna todos os dados de produtos', async () => {
-    const result = await ProductsService.getAll();
+      const ID = 1;
 
-    expect(result).to.be.deep.equal(products)
+      it('retorna um array com o produto esperado', async () => {
+        const result = await ProductsService.findById(ID);
+
+        expect(result).to.be.a('array');
+        expect(result).to.be.deep.equal(product[0]);
+      });
+    });
+
+    describe('caso não exista um produto', () => {
+
+      const ID = 100;
+
+      it('retorna um valor booleano', async () => {
+        const result = await ProductsService.findById(ID);
+
+        expect(result).to.be.a('boolean');
+      });
+
+      it('o booleano deve ser "false"', async () => {
+        const result = await ProductsService.findById(ID);
+
+        expect(result).to.be.equal(false);
+      });
+    });
   });
 });
