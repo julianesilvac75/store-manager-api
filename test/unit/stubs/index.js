cb3e51d25c498101510d40c79fd8a26df7f9a0af
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-07 18:05:51 -0300

    chore: atualize stubs

---
+++ test/unit/stubs/index.js
@@ -0,0 +1,34 @@
+const products = [
+  [
+    {
+      id: 1,
+      name: 'Martelo de Thor',
+      quantity: 10,
+    },
+    {
+      id: 2,
+      name: 'Traje de encolhimento',
+      quantity: 20,
+    },
+    {
+      id: 3,
+      name: 'Escudo do Capitão América',
+      quantity: 30,
+    },
+  ]
+];
+
+const product = [
+  [
+    {
+      id: 1,
+      name: 'Martelo de Thor',
+      quantity: 10,
+    },
+  ]
+];
+
+module.exports = {
+  products,
+  product,
+};
