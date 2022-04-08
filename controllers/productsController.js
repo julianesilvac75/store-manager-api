72cb71964146113bb11ee60c2853f3bf50105445
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-08 20:00:34 -0300

    feat: products - implementing create function on controllers layer

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,8 @@
-const { HTTP_OK_STATUS, HTTP_NOT_FOUND_STATUS } = require('../helpers');
+const {
+  HTTP_OK_STATUS,
+  HTTP_NOT_FOUND_STATUS,
+  HTTP_CONFLICT_STATUS,
+  HTTP_CREATED_STATUS } = require('../helpers');
 const ProductsService = require('../services/productsService');
 
 const getAll = async (req, res) => {
@@ -20,7 +24,21 @@ const findById = async (req, res) => {
   return res.status(HTTP_OK_STATUS).json(product[0]);
 };
 
+const create = async (req, res) => {
+  const { name, quantity } = req.body;
+
+  const newProduct = await ProductsService.create({ name, quantity });
+
+  // Fazer middleware de erro padronizado
+  if (newProduct.error) {
+    return res.status(HTTP_CONFLICT_STATUS).json({ message: 'Product already exists' });
+  }
+
+  return res.status(HTTP_CREATED_STATUS).json(newProduct[0]);
+};
+
 module.exports = {
   getAll,
   findById,
+  create,
 };
