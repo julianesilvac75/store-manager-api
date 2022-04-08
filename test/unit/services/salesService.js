9d509b3b9f44cf7df00d0715add7e948e8ee3520
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-08 17:22:35 -0300

    tests: implementing tests for services layer of endpoint /sales/:id

--- test/unit/services/salesService.js
+++ test/unit/services/salesService.js
@@ -1,7 +1,7 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 
-const { sales } = require('../stubs');
+const { sales, salesById } = require('../stubs');
 const SalesModel = require('../../../models/salesModel');
 const SalesService = require('../../../services/salesService');
 
@@ -20,6 +20,54 @@ describe('Testa a camada Services de sales', () => {
       const result = await SalesService.getAll();
 
       expect(result).to.be.deep.equal(sales[0]);
+    });
+  });
+
+  describe('Ao fazer uma requisição ao endpoint /sales/:id', () => {
+
+    describe('caso existam vendas com o id passado', () => {
+      const ID = 1;
+
+      before(() => {
+        sinon.stub(SalesModel, 'findById').resolves(salesById[0]);
+      });
+
+      after(() => {
+        SalesModel.findById.restore();
+      });
+
+      it('retorna um array com todas as vendas', async () => {
+        const result = await SalesService.findById(ID);
+
+        expect(result).to.be.a('array');
+        expect(result).to.be.deep.equal(salesById[0])
+      });
+    });
+
+    describe('caso não existam vendas', () => {
+
+      const ID = 100;
+      const ERROR = {
+        error: {
+          code: 'Not Found',
+          message: 'Sale not found',
+        },
+      };
+
+      before(() => {
+        sinon.stub(SalesModel, 'findById').resolves(null);
+      });
+
+      after(() => {
+        SalesModel.findById.restore();
+      });
+
+      it('retorna um objeto de erro', async () => {
+        const result = await SalesService.findById(ID);
+
+        expect(result).to.be.a('object');
+        expect(result).to.be.deep.equal(ERROR);
+      });
     });
   });
 });
