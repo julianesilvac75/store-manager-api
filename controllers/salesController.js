b8ee024a6ff36bb8f9b0e28857dcca64f37dfe79
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-08 17:40:17 -0300

    feat: implementing controllers layer for endpoint /sales/:id

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,5 +1,5 @@
 const SalesService = require('../services/salesService');
-const { HTTP_OK_STATUS } = require('../helpers');
+const { HTTP_OK_STATUS, HTTP_NOT_FOUND_STATUS } = require('../helpers');
 
 const getAll = async (req, res) => {
   const sales = await SalesService.getAll();
@@ -7,6 +7,20 @@ const getAll = async (req, res) => {
   return res.status(HTTP_OK_STATUS).json(sales);
 };
 
+const findById = async (req, res) => {
+  const { id } = req.params;
+
+  const salesById = await SalesService.findById(id);
+
+  // Fazer middleware de erro padronizado
+  if (salesById.error) {
+    return res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Sale not found' });
+  }
+
+  return res.status(HTTP_OK_STATUS).json(salesById);
+};
+
 module.exports = {
   getAll,
+  findById,
 };
