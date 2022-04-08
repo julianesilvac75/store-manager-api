94fd36c8e76a807e2ac952ab508df3600874260d
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-08 17:40:55 -0300

    tests: implementing tests for controllers layer of endpoint /sales/:id

--- test/unit/controllers/salesController.js
+++ test/unit/controllers/salesController.js
@@ -1,8 +1,8 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 
-const { HTTP_OK_STATUS } = require('../../../helpers');
-const { sales } = require('../stubs');
+const { HTTP_OK_STATUS, HTTP_NOT_FOUND_STATUS } = require('../../../helpers');
+const { sales, salesById } = require('../stubs');
 const SalesService = require('../../../services/salesService');
 const SalesController = require('../../../controllers/salesController');
 
@@ -35,6 +35,62 @@ describe('Testa a camada Controller de sales', () => {
       await SalesController.getAll(request, response);
 
       expect(response.json.calledWith(sales[0])).to.be.equal(true);
+    });
+  });
+
+  describe('Ao fazer uma requisição ao endpoint /sales/:id', () => {
+    describe('Caso existam vendas com o id passado', () => {
+
+      before(() => {
+        sinon.stub(SalesService, 'findById').resolves(salesById[0]);
+      });
+
+      after(() => {
+        SalesService.findById.restore();
+      });
+
+      it('é chamado o status com o código 200', async () => {
+        await SalesController.findById(request, response);
+
+        expect(response.status.calledWith(HTTP_OK_STATUS)).to.be.equal(true);
+      });
+
+      it('é chamado o json com todas as informações sobre a venda', async () => {
+        await SalesController.findById(request, response);
+        
+        expect(response.json.calledWith(salesById[0])).to.be.equal(true);
+      });
+    });
+
+    describe('Caso não exista nenhuma venda', () => {
+      const ERROR = {
+        error: {
+          code: 'Not Found',
+          message: 'Sale not found',
+        },
+      };
+
+      before(() => {
+        sinon.stub(SalesService, 'findById').resolves(ERROR);
+      });
+
+      after(() => {
+        SalesService.findById.restore();
+      });
+
+      it('é chamado o status com o código 404', async () => {
+        request.params.id = 100;
+
+        await SalesController.findById(request, response);
+
+        expect(response.status.calledWith(HTTP_NOT_FOUND_STATUS)).to.be.equal(true);
+      });
+
+      it('é chamado com o json com a mensagem de erro', async () => {
+        await SalesController.findById(request, response);
+
+        expect(response.json.calledWith({ message: 'Sale not found' })).to.be.equal(true);
+      });
     });
   });
 });
