96f6c79116d3327c79ac666928c43d83aae43962
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-09 13:25:41 -0300

    feat: products - implementing update function on controllers layer

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,7 +2,8 @@ const {
   HTTP_OK_STATUS,
   HTTP_NOT_FOUND_STATUS,
   HTTP_CONFLICT_STATUS,
-  HTTP_CREATED_STATUS } = require('../helpers');
+  HTTP_CREATED_STATUS, 
+  HTTP_BAD_REQUEST_STATUS } = require('../helpers');
 const ProductsService = require('../services/productsService');
 
 const getAll = async (req, res) => {
@@ -37,8 +38,32 @@ const create = async (req, res) => {
   return res.status(HTTP_CREATED_STATUS).json(newProduct);
 };
 
+const update = async (req, res) => {
+  const { id } = req.params;
+  const { name, quantity } = req.body;
+
+  const updatedProduct = await ProductsService.update({ id, name, quantity });
+
+  if (updatedProduct.error) {
+    switch (updatedProduct.error.code) {
+      case 'Not Found':
+        return res
+          .status(HTTP_NOT_FOUND_STATUS).json({ message: 'Product not found' });
+      case 'Bad Request':
+        return res
+          .status(HTTP_BAD_REQUEST_STATUS).json({ message: 'Product could not be updated' });
+      default:
+        return res
+          .status(HTTP_BAD_REQUEST_STATUS).json(updatedProduct.error.message);
+    }
+  }
+
+  return res.status(HTTP_OK_STATUS).json(updatedProduct);
+};
+
 module.exports = {
   getAll,
   findById,
   create,
+  update,
 };
