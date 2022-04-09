3d27f144d0ba5181c1141e868211917319bcbafc
Juliane Cardoso <julianesilvac75@gmail.com>
2022-04-09 12:41:44 -0300

    feat: products - implementing update function on services layer

--- services/productsService.js
+++ services/productsService.js
@@ -38,8 +38,33 @@ const create = async ({ name, quantity }) => {
   return newProduct;
 };
 
+const update = async ({ id, name, quantity }) => {
+  const checkProduct = await ProductsModel.findById(id);
+
+  if (!checkProduct) {
+    return { error: {
+        code: 'Not Found',
+        message: 'Product not found',
+      },
+    };
+  }
+
+  const updatedProduct = await ProductsModel.update({ id, name, quantity });
+
+  if (!updatedProduct) {
+    return { error: {
+        code: 'Bad Request',
+        message: 'Product could not be updated',
+      },
+    };
+  }
+
+  return updatedProduct;
+};
+
 module.exports = {
   getAll,
   findById,
   create,
+  update,
 };
